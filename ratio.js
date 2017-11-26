0c155c5cf16e3529cbc3730ca44442b21f0d1afb
fakefake <35235072+fakefake26@users.noreply.github.com>
2017-11-26 21:10:55 +0100

    refactoring

--- ratio.js
+++ ratio.js
@@ -1,6 +1,12 @@
 // get all tables
 var tables = document.getElementsByClassName('table table-striped');
 
+const GREEN = 'rgb(0, 218, 0)';
+const LIGHT_BLUE = 'rgb(0, 221, 212)';
+const BLUE = 'rgb(20, 29, 216)';
+const ORANGE = 'rgb(235, 153, 0)';
+const RED = 'rgb(255, 29, 29)';
+
 // first for for each tables
 for (var i = tables.length - 1; i >= 0; i--) {
 	// we get all rows of that table from the tbody
@@ -17,17 +23,18 @@ for (var i = tables.length - 1; i >= 0; i--) {
 	var thead = tables[i].getElementsByTagName('thead');
 	// we get the tr of the header
 	var thead_rows = tables[i].getElementsByTagName('tr');
+
 	// header present ?
 	if (thead_rows.length > 0) {
-			// we create the td element for fiability
-			var th = document.createElement('th');
-			th.textContent = 'Fiab%';
-			thead_rows[0].appendChild(th);
+		// we create the td element for the leech %
+		var th = document.createElement('th');
+		th.textContent = 'L%';
+		thead_rows[0].appendChild(th);
 
-			// we create the td element for the leech %
-			var th = document.createElement('th');
-			th.textContent = 'L%';
-			thead_rows[0].appendChild(th);
+		// we create the td element for fiability
+		var th = document.createElement('th');
+		th.textContent = 'F%';
+		thead_rows[0].appendChild(th);
 	}
 
 	// for each row we gonna get the tds values
@@ -35,70 +42,70 @@ for (var i = tables.length - 1; i >= 0; i--) {
 	for (var j = rows.length - 1; j >= 0; j--) {
 		// we get all the cells, we want to retrieve the seed and leech values
 		var cells = rows[j].getElementsByTagName('td');
-
-		// some rows have no data relevant for us
-		if(cells.length == 0) continue;
-
 		// we get the total number of cells
 		var number_of_cells = cells.length;
+
+		// some rows have no data relevant for us
+		if(number_of_cells == 0) continue;
+
 		// we get the seed and leech values in int,
 		// since we want to do a calculus
 		var seed = parseInt(cells[number_of_cells - 2].textContent, 10);
 		var leech = parseInt(cells[number_of_cells - 1].textContent, 10);
 		// the total
 		var total = seed + leech;
-		// we create the td element
-		var td = document.createElement('td');
+
+		// we create the tds elements
+		var td_ratio = document.createElement('td');
+		var td_fiability = document.createElement('td');
+
 		// we get the ratio percentage for the leech
 		if (total == 0) {
 			// 0 so infinite percentage
 			// we avoid a division per zero
-			td.textContent = 'Inf%';
+			td_ratio.textContent = 'Inf%';
 
 			// we color the text
-			td.style.color = 'rgb(255, 29, 29)';
+			td_ratio.style.color = RED;
 
 			// we add a fiability td whose value is not valuable
-			var td_fiability = document.createElement('td');
 			td_fiability.textContent = '-';
 
 			// we add the tds to the corresponding row
+			rows[j].appendChild(td_ratio);
 			rows[j].appendChild(td_fiability);
-			rows[j].appendChild(td);
-		}
-		else {
-			var ratio = ( leech * 100 ) / total;			
-			td.textContent = Math.round(ratio) + '%';
+		} else {
+			var leech_percentage = ( leech * 100 ) / total;
+			var ratio = ( leech * 100 ) / seed;
+			td_ratio.textContent = Math.round(ratio) + '%';
 
 			// we style the text to have a quick view of the 'goodness' of the ratio
-			if (ratio < 20) { td.style.color = 'rgb(0, 218, 0)'; }
-			else if (ratio >= 20 && ratio < 40) { td.style.color = 'rgb(0, 221, 212)'; }
-			else if (ratio >= 40 && ratio < 60) { td.style.color = 'rgb(20, 29, 216)'; }
-			else if (ratio >= 60 && ratio < 80) { td.style.color = 'rgb(235, 153, 0)'; }
-			else if (ratio >= 80) { td.style.color = 'rgb(255, 29, 29)'; }
+			if (ratio < 20) { td_ratio.style.color = GREEN; }
+			else if (ratio >= 20 && ratio < 40) { td_ratio.style.color = LIGHT_BLUE; }
+			else if (ratio >= 40 && ratio < 60) { td_ratio.style.color = BLUE; }
+			else if (ratio >= 60 && ratio < 80) { td_ratio.style.color = ORANGE; }
+			else if (ratio >= 80) { td_ratio.style.color = RED; }
 
 			// we add a fiability to quickly see if the percentage is valuable enough
-			var td_fiability = document.createElement('td');
 			// more than 50 ? ok
 			if (total >= 50) {
 				td_fiability.textContent = "1";
-				td_fiability.style.color = 'rgb(0, 218, 0)';
-			}
-			else {
+				td_fiability.style.color = GREEN;
+			} else {
 				// the fiability revolve around the 50 total seed/leech
 				var fiability = total / 50;
 				// we style the text to have a quick view of the 'goodness' of the fiability
-				if (fiability >= 0.8) { td_fiability.style.color = 'rgb(0, 218, 0)'; }
-				else if (fiability >= 0.6 && fiability < 0.8) { td_fiability.style.color = 'rgb(0, 221, 212)'; }
-				else if (fiability >= 0.4 && fiability < 0.6) { td_fiability.style.color = 'rgb(20, 29, 216)'; }
-				else if (fiability >= 0.2 && fiability < 0.4) { td_fiability.style.color = 'rgb(235, 153, 0)'; }
-				else if (fiability < 0.2) { td_fiability.style.color = 'rgb(255, 29, 29)'; }
+				if (fiability >= 0.8) { td_fiability.style.color = GREEN; }
+				else if (fiability >= 0.6 && fiability < 0.8) { td_fiability.style.color = LIGHT_BLUE; }
+				else if (fiability >= 0.4 && fiability < 0.6) { td_fiability.style.color = BLUE; }
+				else if (fiability >= 0.2 && fiability < 0.4) { td_fiability.style.color = ORANGE; }
+				else if (fiability < 0.2) { td_fiability.style.color = RED; }
 				td_fiability.textContent = fiability.toFixed(2);
 			}
 
 			// we add the tds to the corresponding row
+			rows[j].appendChild(td_ratio);
 			rows[j].appendChild(td_fiability);
-			rows[j].appendChild(td);
 		}
 	}
 }
