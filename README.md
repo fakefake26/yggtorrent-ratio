44faf81d480b2b39e5541cb64372fe1194a54066
fakefake <fake@example.com>
2018-03-25 23:16:18 +0200

    domain change from yggtorrent.com to yggtorrent.is and ygg.is

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # yggtorrent-ratio
-Mozilla Extension which adds a quicker way to see availability of torrents on yggtorrent.com site
+Mozilla Extension which adds a quicker way to see availability of torrents on yggtorrent.is (and ygg.is) site
--- background.js
+++ background.js
@@ -9,7 +9,7 @@ browser.pageAction.onClicked.addListener(handleClick);
 
 // we show the icon in the address bar only when we are on the yggtorrent sites
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.url.match('^https?://([0-9a-zA-Z\-]+\.)?yggtorrent\.com/.*$')) {
+    if (tab.url.match('^https?://([0-9a-zA-Z\-]+\.)?ygg(torrent)?\.(is|com)/.*$')) {
         browser.pageAction.show(tab.id);
     }
 });
